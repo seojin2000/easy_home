1ffe0d0a3569c7e3e5a5cc2974ca4641498786b8
gyeolhan0789 <gyeolhan0789@gmail.com>
2025-02-11 23:42:51 +0900

    Feat: msa-sb-user 초기 설정

---
+++ msa-sb-user/src/main/java/org/example/msasbuser/MsaSbUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.msasbuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSbUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaSbUserApplication.class, args);
+    }
+
+}
