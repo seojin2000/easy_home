7c80a780e736798a72907a7ce5eca250b9d50f0e
gyeolhan0789 <gyeolhan0789@gmail.com>
2025-02-12 00:06:27 +0900

    Feat: msa-sb-user 초기 설정

--- msa-sb-user/src/main/java/org/example/msasbuser/MsaSbUserApplication.java
+++ msa-sb-user/src/main/java/org/example/msasbuser/MsaSbUserApplication.java
@@ -1,5 +1,6 @@
 package org.example.msasbuser;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsaSbUserApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        String dbHost = dotenv.get("JWT_SECRET_KEY");
         SpringApplication.run(MsaSbUserApplication.class, args);
     }
 
